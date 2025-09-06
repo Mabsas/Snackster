2684be5ab15f926c1287a9204e0dff028992c6ac
Mabsas <139257914+Mabsas@users.noreply.github.com>
2025-09-06 20:31:20 +0600

    User & Admin Login added

--- Snackster/User/Login.aspx.cs
+++ Snackster/User/Login.aspx.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,14 +13,54 @@ namespace Snackster.User
 {
     public partial class Login : System.Web.UI.Page
     {
+
+        SqlConnection con;         //con → database connection
+        SqlCommand cmd;           //cmd → SQL command (insert/update/select)
+        SqlDataAdapter sda;    //sda + dt → usually for SELECT queries
+        DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["userId"] != null)
+            {
+                Response.Redirect("Default.aspx");
+            }
 
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+            if(txtUsername.Text.Trim() == "Admin" && txtPassword.Text.Trim() == "123")
+            {
+                Session["admin"] = txtUsername.Text.Trim();
+                Response.Redirect("../Admin/Dashboard.aspx");
+            }
 
+            else
+            {
+                con = new SqlConnection(Connection.GetConnectionString());
+                cmd = new SqlCommand("User_Crud", con);
+                cmd.Parameters.AddWithValue("@Action", "SELECT4LOGIN");
+                cmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                cmd.Parameters.AddWithValue ("@Password", txtPassword.Text.Trim());
+                cmd.CommandType = CommandType.StoredProcedure;
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sda.Fill(dt);
+
+                if(dt.Rows.Count == 1)
+                {
+                    Session["username"] = txtUsername.Text.Trim().Trim();
+                    Session["userId"] = dt.Rows[0]["UserId"];
+                    Response.Redirect("Default.aspx");
+                }
+                else
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Invalid Credentials....!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
         }
     }
 }
