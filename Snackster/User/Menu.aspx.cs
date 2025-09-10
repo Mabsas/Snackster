b0c74c989ff648f3e62e66b8a41b51a8faa76d19
zarif50 <zarifmahmud250@gmail.com>
2025-09-10 23:06:41 +0600

    profile and Menu Backend fetch

--- Snackster/User/Menu.aspx.cs
+++ Snackster/User/Menu.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Snackster.Admin;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,45 @@ namespace Snackster.User
 {
     public partial class Menu : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!IsPostBack)
+            {
+                getProducts();
+                getCategories();
+            }
         }
+        private void getCategories()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "ACTIVECAT");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rCategory.DataSource = dt;
+            rCategory.DataBind();
+        }
+        private void getProducts()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Product_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "ACTIVEPROD");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rProducts.DataSource = dt;
+            rProducts.DataBind();
+        }
+        //public string LowerCase(Object obj)
+        //{
+        //    return obj.ToString().ToLower();
+        //}
     }
 }
