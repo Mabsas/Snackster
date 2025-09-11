d39d759515a8903fd80c7b337b34cfb1fb02acef
zarif50 <zarifmahmud250@gmail.com>
2025-09-11 22:39:03 +0600

    Cart added to item

--- Snackster/Connection.cs
+++ Snackster/Connection.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
 using System.Linq;
 using System.Security.Policy;
 using System.Web;
@@ -19,6 +22,9 @@ namespace Snackster
     // Checks for valid image extensions
     public class Utils
     {
+        SqlConnection con;
+        SqlCommand cmd;
+
         public static bool IsValidExtension(string fileName)
         {
             bool isValid = false;
@@ -50,6 +56,33 @@ namespace Snackster
             }
 
             return url1;
+        }
+
+        public bool updateCartQuantity(int quantity, int productId, int userId)
+        {
+            bool isUpdated = false;
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Cart_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "UPDATE");
+            cmd.Parameters.AddWithValue("@ProductId",productId);
+            cmd.Parameters.AddWithValue("@Quantity", quantity);
+            cmd.Parameters.AddWithValue("@UserId",userId);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                isUpdated = true;
+            }
+            catch (Exception ex)
+            {
+               System.Web.HttpContext.Current.Response.Write("<script>alert('Error-" + ex.Message + "');<script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+            return isUpdated;
         }
     }
 }
