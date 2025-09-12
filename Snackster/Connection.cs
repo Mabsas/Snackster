8bd97eadb496af71573f6a2011ef1585fab41ddc
zarif50 <zarifmahmud250@gmail.com>
2025-09-12 23:10:43 +0600

    Payment Method Updated

--- Snackster/Connection.cs
+++ Snackster/Connection.cs
@@ -99,5 +99,12 @@ namespace Snackster
             sda.Fill(dt);   
             return dt.Rows.Count;
         }
+        public static string GetUniqueId()
+        {
+            Guid guid = Guid.NewGuid();
+            String uniqueId = guid.ToString();
+            return uniqueId;
+        }
     }
+
 }
